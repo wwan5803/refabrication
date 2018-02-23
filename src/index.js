dc5e34478da3932700ce07eb80eaacd94fb8e287
wesley.wang <wesley.wang@zerologix.com>
2018-02-23 18:09:47 +1100

    update nav

--- src/index.js
+++ src/index.js
@@ -3,17 +3,21 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import Home from "./home";
 import About from "./about";
+import Nav from "./nav";
 import registerServiceWorker from "./registerServiceWorker";
-import { BrowserRouter as Router, Link } from "react-router-dom";
-import { Switch, Route } from "react-router";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+// import { Switch, Route } from "react-router";
 
 ReactDOM.render(
-  <Router>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-    </Switch>
-  </Router>,
+  <div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+      </Switch>
+    </Router>
+    <Nav />
+  </div>,
   document.getElementById("root")
 );
 registerServiceWorker();
--- src/nav/index.js
+++ src/nav/index.js
@@ -1,3 +1,25 @@
-/**
- * Created by WesleyWang on 2/23/2018.
- */
+import React, { Component } from "react";
+import "./nav.css";
+
+export default class Nav extends Component {
+  render() {
+    return (
+      <div className="nav">
+        <div className="logo">
+          <img
+              style={{marginLeft: 30}}
+            src="https://www.upgrowth.com.au/static/media/text-logo.3a56559d.svg"
+          />
+        </div>
+        <div className="links">
+          <div>Experience Design</div>
+          <div>Tech delivery</div>
+          <div>Business design</div>
+        </div>
+        <div className="contact">
+          <button>Get in touch</button>
+        </div>
+      </div>
+    );
+  }
+}
